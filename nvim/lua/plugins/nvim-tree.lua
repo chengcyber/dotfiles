f1f1c8b0d2660b9ec54798b4f64f984790e252f6
Cheng Liu <liucheng.tech@outlook.com>
2021-10-18 17:27:59 +0800

    feat(nvim): nvim tree config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -4,10 +4,14 @@ local nnoremap = vimp.nnoremap
 require('nvim-tree').setup({
   disable_netrw = false,
   hijack_netrw = false,
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+  },
   view = {
     side = 'right',
     auto_resize = true,
-  }
+  },
 })
 
 nnoremap('<leader>op', ':NvimTreeToggle<CR>')
